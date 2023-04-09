d28e460574213c571c582a5ea042b1a8f4398b3b
miraaydurgun <miraydurgun67@gmail.com>
2023-04-09 19:13:19 +0300

    Blog ekleme yapıldı.

--- CoreBlog/Controllers/BlogController.cs
+++ CoreBlog/Controllers/BlogController.cs
@@ -1,5 +1,9 @@
-﻿using BusinessLayer.Concrete;
+﻿using System;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationsRules;
 using DataAccessLayer.EntityFramwork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,8 +32,38 @@ namespace CoreDemo.Controllers
 		public IActionResult BlogListByWriter()
 		{
 			//writer idye göre getirir
-			var values = bm.GetBlogListByWriter(5);
+			var values = bm.GetBlogListByWriter(1);
 			return View(values);
+		}
+		[HttpGet]
+		public IActionResult BlogAdd()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult BlogAdd(Blog p)
+		{
+			BlogValidator bv = new BlogValidator();
+			ValidationResult results = bv.Validate(p);
+
+			if (results.IsValid)
+			{
+				p.BlogStatus = true; //false yaparak admine onaylatabiliriz
+				p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToLongDateString());
+				p.WriterID = 1;
+				bm.TAdd(p);
+				return RedirectToAction("BlogListByWriter", "Blog");
+
+
+			}
+			else
+			{
+				foreach (var item in results.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
 		}
 	}
 }
