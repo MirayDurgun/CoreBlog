f04c41f2bdef108755d0f407b16e72ef7c8bce37
miraaydurgun <miraydurgun67@gmail.com>
2023-08-14 22:12:17 +0300

    70.Video sonu

--- CoreBlog/Controllers/DashBoardController.cs
+++ CoreBlog/Controllers/DashBoardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
@@ -7,6 +9,15 @@ namespace CoreBlog.Controllers
 	{
 		[AllowAnonymous]
 		public IActionResult Index()
+		{
+			Context c=new Context();
+			ViewBag.v1=c.Blogs.Count().ToString();
+			ViewBag.v2=c.Blogs.Where(x=>x.WriterID==1).Count();
+			ViewBag.v3=c.Categories.Count();
+			return View();
+		}
+		[AllowAnonymous]
+		public IActionResult v()
 		{
 			return View();
 		}
