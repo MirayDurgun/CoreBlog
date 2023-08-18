938dd90c60371a698102253fbfcb970d4af95f07
mdurgun <mdurgun@KZYAZILIM10>
2023-08-18 09:07:15 +0300

    79. Video sonu

--- CoreBlog/Controllers/NotificationController.cs
+++ CoreBlog/Controllers/NotificationController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramwork;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult AllNotification()
+        {
+            var values=nm.GetList();
+            return View(values);
+        }
     }
 }
+
