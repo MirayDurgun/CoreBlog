bf3a4345b6126a82450d2be72695b8a318617d54
Miray Durgun <miraydurgun67@gmail.com>
2023-09-11 21:26:14 +0300

    122. Video sonu

---
+++ CoreBlog/Controllers/EmployeeTestController.cs
@@ -0,0 +1,98 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        //Presentation katmanu üzerinden api datasını erişip CRUD işlemleri gerçekleştireceğiz
+        //BlogApi'yi referans gösterdik
+        public async Task<IActionResult> Index()
+        {
+            //göndermiş olduğumuz apinin adresinde(localhost) bulunan adrese istekte bulunacağız
+            //ve bu isteğe karşılık gelen değerleri listeleyeceğiz
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync(""); //adresi yaz!HTTP GET İsteği Gönderme
+            var jsonString = await responseMessage.Content.ReadAsStringAsync(); //HTTP yanıtı okunuyor
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString); //JSON veri, belirtilen Class1 sınıfına uygun bir şekilde deserialize ediliyor.
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var rensonseMessage = await httpClient.PostAsync("", content);
+            if (rensonseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var rensonseMessage = await httpClient.GetAsync("" + id);
+            if (rensonseMessage.IsSuccessStatusCode)
+            {
+                var jsonEmployee = await rensonseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<Class1>(jsonEmployee);
+                return View(values);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var rensonseMessage = await httpClient.PutAsync("", content);
+            if (rensonseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var httpClient = new HttpClient();
+            var rensonseMessage = await httpClient.DeleteAsync("" + id);
+            if (rensonseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+    /*DeserializeObject
+        JSON verilerini bir .NET nesnesine dönüştürmeye olanak tanır.
+      SerializeObject
+       .NET nesnesini JSON verisine çevirir.
+    */
+
+
+    public class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Concrete
 {
     //bağlantı adresi tanımlanacak bu kısımda
-    public class Context : DbContext  /*DbContext clasından miras alıyor
-                                      DbContext sınıfını kullanabilmek için EntityFrameworkCore paketi dahil edilmeli*/
+    public class Context : IdentityDbContext<AppUser>
+
+    /*DbContext clasından miras alıyor
+      DbContext sınıfını kullanabilmek için EntityFrameworkCore paketi dahil edilmeli
+    
+    Identity'de DbContext yerine IdentityDbContext kullanılır*
+    IdentityDbContext, Context sınıfından miras alır ve Context sınıfına ait bütün özellikleri kullanabilir*/
+
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //2 şekilde bağlantı kurulur 
             //1.
             optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; database=CoreBlogDb; integrated security=true;");//2 adet "\\" olmalı
-                                                                                                                          //2.
-                                                                                                                          // optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB; database=CoreBlogDb; integrated security=true;");
-            /*Bu şekilde de bağlantı sağlanabilir, connectionString  hangi ana makine'ye bağlantı yapılacağını,
+
+            /*2.                                                                                                           optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB; database=CoreBlogDb; integrated security=true;");
+            Bu şekilde de bağlantı sağlanabilir, connectionString  hangi ana makine'ye bağlantı yapılacağını,
             o ana makinedeki hangi veritabanına bağlanacağımızı, o veritabanına bağlanmak için gerekli olan
             kullanıcı adı ve şifresi gibi bilgilerin tutulduğu bir kod parçasıdır.*/
 
@@ -39,6 +45,7 @@ namespace DataAccessLayer.Concrete
                 .HasForeignKey(z => z.ReceiverID)
                 .OnDelete(DeleteBehavior.ClientSetNull);
 
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<About> Abouts { get; set; } //veritababnında tabloda isimler about yerine abouts oalrak görünecek. karışmaması için
         public DbSet<Blog> Blogs { get; set; }
