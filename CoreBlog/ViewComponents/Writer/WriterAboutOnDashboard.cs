e5804d7128737b7099eb3a71cef9f4d9ccae27df
Miray Durgun <miraydurgun67@gmail.com>
2023-09-12 13:28:43 +0300

    126. Video sonu

--- CoreBlog/ViewComponents/Writer/WriterAboutOnDashboard.cs
+++ CoreBlog/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -1,24 +1,29 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreBlog.ViewComponents.Writer
 {
-	public class WriterAboutOnDashboard : ViewComponent
-	{
-		WriterManager writerManager = new WriterManager(new EfWriterRepository());
+    public class WriterAboutOnDashboard : ViewComponent
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        Context c = new Context();
 
-		Context c = new Context();
-
-		public IViewComponentResult Invoke()
-		{
-			var userMail = User.Identity.Name;
-			var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
-			var values = writerManager.GetWriterByID(writerID);
-			return View(values);
-		}
-	}
+        public IViewComponentResult Invoke()
+        {
+            var userName = User.Identity.Name;
+            ViewBag.name = userName;
+            var userMail = c.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+            var values = writerManager.GetWriterByID(writerID);
+            return View(values);
+        }
+    }
 }
