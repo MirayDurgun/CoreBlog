a00c39d7d3924cb7bdc586e3287c413dd3e78cbb
Miray Durgun <miraydurgun67@gmail.com>
2023-09-13 13:12:05 +0300

    135. Video sonu

---
+++ CoreBlog/Areas/Admin/Controllers/AdminMessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.Areas.Admin.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+        public IActionResult SendBox()
+        {
+            return View();
+        }
+    }
+}
