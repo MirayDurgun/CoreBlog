e09b6c02adc8b78c243b1c818cc13bd25e3cb105
Miray Durgun <miraydurgun67@gmail.com>
2023-09-13 15:33:31 +0300

    137. Video 6.28

--- CoreBlog/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreBlog/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using System;
 using System.Linq;
 
 namespace CoreBlog.Areas.Admin.Controllers
@@ -32,9 +35,26 @@ namespace CoreBlog.Areas.Admin.Controllers
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 message2)
+        {
+            var userName = User.Identity.Name;
+            var userMail = context.Users.Where(x => x.UserName == userName).Select(y => y.Email).FirstOrDefault();
+            var writerID = context.Writers.Where(x => x.WriterMail == userMail).Select(y => y.WriterID).FirstOrDefault();
+
+            message2.SenderID = writerID;
+            //message2.ReceiverID=2;
+            message2.MessageStatus = true;
+            message2.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            mm2.TAdd(message2);
+            return RedirectToAction("SendBox");
         }
     }
 }
