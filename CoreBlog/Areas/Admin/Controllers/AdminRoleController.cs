b97f9f7cbe840f0f90101c4b9d1a68df196262b5
Miray Durgun <miraydurgun67@gmail.com>
2023-09-14 13:32:41 +0300

    139. Video sonu Yeni rol ekleme yapıldı. View ve controller oluşturuldu

--- CoreBlog/Areas/Admin/Controllers/AdminRoleController.cs
+++ CoreBlog/Areas/Admin/Controllers/AdminRoleController.cs
@@ -1,7 +1,9 @@
-﻿using EntityLayer.Concrete;
+﻿using CoreBlog.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreBlog.Areas.Admin.Controllers
 {
@@ -19,6 +21,36 @@ namespace CoreBlog.Areas.Admin.Controllers
         {
             var values = _roleManager.Roles.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel rolemodel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole
+                {
+                    Name = rolemodel.name
+                };
+
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(rolemodel);
         }
     }
 }
---
+++ CoreBlog/Models/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreBlog.Models
+{
+    public class RoleViewModel
+    {
+        [Display(Name = "Rol Adı")]
+        [Required(ErrorMessage = "Lütfen rol adınızı giriniz")]
+        public string name { get; set; }
+    }
+}
