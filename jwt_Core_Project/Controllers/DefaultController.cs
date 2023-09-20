7b889e7d5e047fddd00a9cf7838eb46c54aec8e9
Miray Durgun <miraydurgun67@gmail.com>
2023-09-20 19:34:47 +0300

    147. Video sonu

--- jwt_Core_Project/Controllers/DefaultController.cs
+++ jwt_Core_Project/Controllers/DefaultController.cs
@@ -8,14 +8,14 @@ namespace jwt_Core_Project.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
-        [HttpGet("action")]
+        [HttpGet("[action]")]
         public IActionResult Login()
         {
             return Created("", new BuildToken().CreateToken());
         }
 
         [Authorize]
-        [HttpGet("action")]
+        [HttpGet("[action]")]
         public IActionResult Page1()
         {
             return Ok("Sayfa 1 İçin Girişiniz Başarılı");
--- jwt_Core_Project/DAL/BuildToken.cs
+++ jwt_Core_Project/DAL/BuildToken.cs
@@ -9,10 +9,10 @@ namespace jwt_Core_Project.DAL
     {
         public string CreateToken()
         {
-            var bytes = Encoding.UTF8.GetBytes("aspnetcoreprojekampi");
+            var bytes = Encoding.UTF8.GetBytes("aspnetprojekampi");
             SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
-            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.Aes128CbcHmacSha256);
-            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore:DateTime.Now,expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
             JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
             return handler.WriteToken(token);
         }
